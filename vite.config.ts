f7e6970d2c6c580b8842ff3a26cf366f686b0b3e
Joud73 <145606806+Joud73@users.noreply.github.com>
2026-01-07 14:58:45 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/tamkeen-HRSD/",
+  // IMPORTANT for GitHub Pages repo sites:
+  // production => /<repo-name>/
+  // dev => /
+  base: mode === "production" ? "/tamkeen-HRSD/" : "/",
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
